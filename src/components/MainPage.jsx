376a581f1ea48edcce2f63f30c4ecb20e130326e
Ali <muhammet.ali.safak@gmail.com>
2025-03-17 17:53:54 +0300

    sayfa temelleri atıldı

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,22 @@
+export default function MainPage() {
+  return (
+    <>
+      <div className="container">
+        <div className="header">
+          <button>giriş yap</button>
+          <button>kayıt ol</button>
+        </div>
+
+        <div className="hero">
+          <h3>Burada rock papper </h3>
+          <button>geri</button>
+          <button>ileri</button>
+        </div>
+
+        <div className="footer">
+          
+        </div>
+      </div>
+    </>
+  )
+}
