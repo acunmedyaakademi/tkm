0f2b783f097f5f22ce093077160cd1d61a0ea092
Berna Kurt <beekurt98@gmail.com>
2025-03-18 17:07:15 +0300

    supabase giriş kayıt ve çıkış eklendi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,83 @@
+import { useContext, useRef } from "react"
+import { UserContext } from "../App";
+
 export default function Header() {
+  const { supabase, authUser, setAuthUser } = useContext(UserContext);
+  const signUpRef = useRef(null);
+  const loginRef = useRef(null);
 
-  async function handleSignup() {
+  async function handleSignup(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formObj = Object.fromEntries(formData);
+    const { name, email, password } = formObj;
+    const options = { data: { name } }
     const { data, error } = await supabase.auth.signUp({
-      email: 'example@email.com',
-      password: 'example-password',
+      email,
+      password,
+      options
     })
-    
+    signUpRef.current.close();
   }
 
-  function handleLogin() {
-
+  async function handleLogin(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formObj = Object.fromEntries(formData);
+    const { email, password } = formObj;
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+    loginRef.current.close();
   }
- 
+
+  async function handleLogout(e) {
+    e.preventDefault();
+    const { error } = await supabase.auth.signOut()
+  }
+
 
   return (
     <>
-    <div className="header-cont">
-      <img src="public\imgs\rps-logo.png" alt="" />
-      <div className="user-controls">
-        <button onClick={() => handleLogin()} className="login">Giriş Yap</button>
-        <button onClick={() => handleSignup()} className="signup">Kayıt Ol</button>
+      <div className="header-cont">
+        <img src="public\imgs\rps-logo.png" alt="" />
+        <div className="user-controls">
+          {
+            authUser
+              ? <>
+                <p>{authUser.name}</p>
+                <button onClick={handleLogout}>Sign Out</button>
+              </>
+              : <>
+                <button onClick={() => loginRef.current.showModal()} className="login">Giriş Yap</button>
+                <button onClick={() => signUpRef.current.showModal()} className="signup">Kayıt Ol</button>
+
+              </>
+
+          }
+        </div>
       </div>
-    </div>
-    <dialog open>
-      dialog
-    </dialog>
-      
+
+      <dialog ref={signUpRef}>
+        <h3>Kayıt Ol</h3>
+        <form className="user-control-form" onSubmit={handleSignup}>
+          <input type="text" placeholder="İsim" name="name" />
+          <input type="text" placeholder="E-mail" name="email" />
+          <input type="password" placeholder="Parola" name="password" />
+          <button>Kayıt Ol</button>
+        </form>
+      </dialog>
+
+      <dialog ref={loginRef}>
+        <h3>Giriş Yap</h3>
+        <form className="user-control-form" onSubmit={handleLogin}>
+          <input type="text" placeholder="E-mail" name="email" />
+          <input type="password" placeholder="Parola" name="password" />
+          <button>Giriş Yap</button>
+        </form>
+      </dialog>
+
     </>
   )
 
